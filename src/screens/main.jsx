02e3e57622e6b9e683d5dccc01441b9d02d88849
Abdulbosid <akamalov207@gmail.com>
2023-06-24 11:56:30 +0600

    1

--- src/screens/main.jsx
+++ src/screens/main.jsx
@@ -55,7 +55,7 @@ const Main = () => {
             </div>
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-12 col-lg-4 mt-2 tm">
+                    <div className="col-xs-12 col-lg-4 mt-2 tm">
                         <ins><a href="" className="text-primary"><h3>Что популярно</h3></a></ins>
                     </div>
                     <div className="col-12 over d-flex tm">
@@ -83,7 +83,7 @@ const Main = () => {
                             </>
                         }
                     </div>
-                    <div className="col-sm-12 col-lg-4 text-primary mt-2 tm">
+                    <div className="col-xs-12 col-lg-4 text-primary mt-2 tm">
                         <h3>Последние трейлеры</h3>
                     </div>
                     <div className="col-12 text-primary tm">
@@ -91,40 +91,40 @@ const Main = () => {
                             <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
-                            <div className="col-2 mt-2">
+                            <div className="col-xs-6 col-lg-2 mt-2">
                                 <iframe className="rounded" width="180" height="250" src="https://www.youtube.com/embed/hq3C0qimFjs" title="Таких Нокаутов Больше Не Увидишь! Мирко Крокоп в Кикбоксинге" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
                         </div>
                     </div>
-                    <div className="col-4 mt-2 tm">
+                    <div className=" col-xs-12 col-lg-4 mt-2 tm">
                         <ins><a href="" className="text-primary"><h3>в тренде</h3></a></ins>
                     </div>
                     <div className="col-12 over d-flex tm">
                         {movies.length > 0 ?
                             <>
                                 {movie.map((item) =>
-                                    <a className="col-sm-3 col-xl-2 clip" href={'/g4/' + item.id}>
+                                    <a className="col-xs-6 col-xl-2 clip" href={'/g4/' + item.id}>
                                         <Card
                                             hoverable
                                             style={{
