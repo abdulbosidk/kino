79a8a83d6d68ce7c9cbb2fb0da343ef78dadc04b
Abdulbosid <akamalov207@gmail.com>
2023-06-24 13:01:10 +0600

    11

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                         <div className="row">
                             <div className="col-12 col-lg-4"></div>
                             <div className="col-12 col-lg-4 ">
-                                <img width={200} className="mb-2" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" />
+                                <img width={'100%'} className="mb-2" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" />
                             </div>
                             <div className="col-12 col-lg-4"></div>
                         </div>
