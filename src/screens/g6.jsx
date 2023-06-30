3917c050dea35dd90bd45a8003fe528434651aa0
Abdulbosid <akamalov207@gmail.com>
2023-06-30 11:14:38 +0600

    1

--- src/screens/g6.jsx
+++ src/screens/g6.jsx
@@ -8,10 +8,11 @@ const Good6 = () => {
     const params = useParams();
     const id = params.id;
     const [movie, setMovie] = useState(null);
+    const apikey = '3cc05ada7e70628b8d1bf36e4d1f6fd7&';
     const fetchMovie = async () => {
         const abdulbosid = await axios({
             method: "get",
-            url: `https://api.themoviedb.org/3/person/popular?api_key=3cc05ada7e70628b8d1bf36e4d1f6fd7&`
+            url: `https://api.themoviedb.org/3/person/${id}?api_key=${apikey}&language=en-US`
         })
         console.log('abdulbosid', abdulbosid);
         if (abdulbosid.status == 200) {
@@ -29,7 +30,7 @@ const Good6 = () => {
                     <div className="row">
                         <div className="col-12 mt-3 tm">
                             <div className="row">
-                                <div className="col-xl-4 col-sm-4">
+                                <div className="col-lg-4 col-sm-12">
                                     <img width={'100%'} src={"https://www.themoviedb.org/t/p/w235_and_h235_face/" + movie.profile_path} />
                                 </div>
                                 <div className="col-sm-8 col-xl-8 p-2">
